8641146fc4dea0a9248fa6cd92de64c2944168b9
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-06-29 18:17:48 -0300

    refact

--- src/components/Total/Total.test.tsx
+++ src/components/Total/Total.test.tsx
@@ -5,36 +5,36 @@ import { describe, it, expect } from 'vitest'
 
 describe('Total Component', () => {
     it('renders the Total label', () => {
-        render(<Total quantities={[]} denominations={[]} />);
-        const totalLabel = screen.getByText(/Total/);
-        expect(totalLabel).toBeInTheDocument();
-    });
+        render(<Total quantities={[]} denominations={[]} />)
+        const totalLabel = screen.getByText(/Total/)
+        expect(totalLabel).toBeInTheDocument()
+    })
 
     it('calculates and displays the correct total', () => {
-        const quantities = ['2', '1', '5'];
-        const denominations = [20000, 10000, 2000];
+        const quantities = ['2', '1', '5']
+        const denominations = [20000, 10000, 2000]
 
-        render(<Total quantities={quantities} denominations={denominations} />);
+        render(<Total quantities={quantities} denominations={denominations} />)
 
         const totalSpan = screen.getByText((content) => {
-        const cleaned = content.replace(/\./g, '').replace(',', '.');
-        return cleaned.includes('60000');
-        });
+        const cleaned = content.replace(/\./g, '').replace(',', '.')
+        return cleaned.includes('60000')
+        })
 
-        expect(totalSpan).toBeInTheDocument();
-    });
+        expect(totalSpan).toBeInTheDocument()
+    })
 
     it('handles empty or invalid quantities gracefully', () => {
-        const quantities = ['', 'abc', undefined as any];
-        const denominations = [20000, 10000, 2000];
+        const quantities = ['', 'abc', undefined as any]
+        const denominations = [20000, 10000, 2000]
 
-        render(<Total quantities={quantities} denominations={denominations} />);
+        render(<Total quantities={quantities} denominations={denominations} />)
 
         const totalSpan = screen.getByText((content) => {
-        const cleaned = content.replace(/\./g, '').replace(',', '.');
-        return cleaned.includes('0');
-        });
+        const cleaned = content.replace(/\./g, '').replace(',', '.')
+        return cleaned.includes('0')
+        })
 
-        expect(totalSpan).toBeInTheDocument();
-    });
-});
+        expect(totalSpan).toBeInTheDocument()
+    })
+})
